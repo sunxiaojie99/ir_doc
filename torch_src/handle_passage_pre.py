bab8ec144968f8dbf45f36e5046155f47516f9b1
sunxj <505932690@qq.com>
2022-05-08 20:27:16 +0800

    fix handle_passage_pre

--- torch_src/handle_passage_pre.py
+++ torch_src/handle_passage_pre.py
@@ -3,6 +3,7 @@ import json
 from regex import P
 from tqdm import tqdm
 from tokenization import _is_whitespace, _is_control
+import json
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,7 +22,7 @@ def _clean_text(text):
     return "".join(output)
 
 
-def handle(data_dir, map_file, out_file):
+def handle(data_dir, map_file, out_file, new_map_file):
     """
     passage文件中，有的passage比如376574行，全部由控制字符组成，需要提前过滤掉.
     eg: "\x07\x07\x07\x05\x07"
@@ -38,23 +39,31 @@ def handle(data_dir, map_file, out_file):
     new_passage_lineidx2id = {}
     count = 0  # 原始行👌
     new_count = 0
+    print(file_name_list)
     for file_name in file_name_list:
         if "part" in file_name:
             file = os.path.join(data_dir, file_name)
             with open(file, "r", encoding="utf-8") as f:
                 lines = f.readlines()
                 for l in tqdm(lines):
+                    passage_id = passage_lineidx2id[str(count)]  # 获取旧的pid
                     count += 1
-                    clean_text = _clean_text(l.strip())
+                    line = l.strip().split('\t')
+                    passage = line[2]
+                    clean_text = _clean_text(passage)
                     if clean_text == "":
                         continue
-                    new_count += 1
+                    
                     f_w.write(l)
-                    new_passage_lineidx2id[str(
-                        new_count)] = passage_lineidx2id[str(count)]
+                    new_passage_lineidx2id[str(new_count)] = passage_id  # 存入pid
+                    new_count += 1
 
     f_w.close()
     print('新的文章数：', new_count)
+
+    with open(new_map_file, 'w', encoding='utf-8') as f_out:
+        json.dump(new_passage_lineidx2id, f_out, ensure_ascii=False, indent=2)
+    
 
 
 def merge(data_dir, out_file):
@@ -77,6 +86,8 @@ def merge(data_dir, out_file):
 handle(os.path.join(here, "../dureader-retrieval-baseline-dataset/passage-collection/"),
        os.path.join(
            here, "../dureader-retrieval-baseline-dataset/passage-collection/passage2id.map.json"),
-       os.path.join(here, "../dureader-retrieval-baseline-dataset/passage-collection/all_doc"))
+       os.path.join(here, "../dureader-retrieval-baseline-dataset/passage-collection/all_doc"),
+       os.path.join(
+           here, "../dureader-retrieval-baseline-dataset/passage-collection/new_passage2id.map.json"))
 # merge(os.path.join(here, "../dureader-retrieval-baseline-dataset/passage-collection/"),
 #       os.path.join(here, "../dureader-retrieval-baseline-dataset/passage-collection/all_doc"))
